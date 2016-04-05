b515df81caa6f16c5277b23ba6e1c1e8b8401892
Maarten Veerman <M.T.J.Veerman@uva.nl>
2016-04-05 14:20:32 +0000

    OOPOCTVWO-35, OOPOCTVWO-39 - routing now always starts with assignment1
    git-svn-id: https://source.ic.uva.nl/repos/svn/odg-1/virtualmath/virtualmath/trunk@9093 89e93b6c-07a7-4196-abc3-db709d5d5625
    
    Former-commit-id: 20153a321fa94e08f09994e9689f1105064235c6

--- app/app.js
+++ app/app.js
@@ -17,29 +17,44 @@ angular.module('virtualMath', [
     }])
 
     // general survey controller
-    .controller('surveyController', function ($scope) {
+    .controller('surveyController', function ($scope, $rootScope) {
+        $rootScope.initialized = false;
+
         $scope.surveyData = {
             runs: [],
             currentRun: -1,
             lastScore: -1
         };
 
-        $scope.getSurveyRunData = function() {
+        $scope.getSurveyRunData = function () {
             return $scope.surveyData.runs;
         };
 
-        $scope.getCurrentSurveyRunData = function() {
+        $scope.getCurrentSurveyRunData = function () {
             if ($scope.surveyData.currentRun < 0) {
                 $scope.startNewSurveyRun();
+                $rootScope.initialized = true;
             }
 
             return $scope.surveyData.runs[$scope.surveyData.currentRun];
         };
 
-        $scope.startNewSurveyRun = function() {
+        $scope.startNewSurveyRun = function () {
             $scope.surveyData.runs.push({
                 score: -1
             });
             $scope.surveyData.currentRun++;
         };
+    })
+
+    .run( function($rootScope, $location) {
+
+        // register listener to watch route changes
+        $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+            if ( !$rootScope.initialized ) {
+                if ( next.templateUrl != "/screens/assignment1/assignment1.html" ) {
+                    $location.path( "/assignment1" );
+                }
+            }
+        });
     });
