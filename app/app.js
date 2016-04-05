4db90f69fd0a85f1fa4beb35e2b503fcb7bc52cf
Maarten Veerman <M.T.J.Veerman@uva.nl>
2016-04-05 08:37:10 +0000

    OOPOCTVWO-35 - first draft on surveyStorage
    git-svn-id: https://source.ic.uva.nl/repos/svn/odg-1/virtualmath/virtualmath/trunk@9089 89e93b6c-07a7-4196-abc3-db709d5d5625
    
    Former-commit-id: 4a06d2c46876762fb0c9c9ecaf7f3ad48b609ae2

--- app/app.js
+++ app/app.js
@@ -2,14 +2,44 @@
 
 // Declare app level module which depends on views, and components
 angular.module('virtualMath', [
-    'ngRoute',
-    'ngTouch',
-    'virtualMath.assignment1',
-    'virtualMath.assignment2',
-    'virtualMath.help1',
-    'virtualMath.compare',
-    'virtualMath.version'
+        'ngRoute',
+        'ngTouch',
+        'virtualMath.assignment1',
+        'virtualMath.assignment2',
+        'virtualMath.help1',
+        'virtualMath.compare',
+        'virtualMath.version'
+    ])
 
-]).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/assignment1'});
-}]);
+    // config
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/assignment1'});
+    }])
+
+    // general survey controller
+    .controller('surveyController', function ($scope) {
+        $scope.surveyData = {
+            runs: [{
+                score: -1
+            }],
+            currentRun: 0,
+            lastScore: -1
+        };
+
+        $scope.getSurveyRunData = function() {
+            return $scope.surveyData.runs;
+        };
+
+        $scope.getCurrentSurveyRunData = function() {
+            if ($scope.surveyData.runs.length > $scope.surveyData.currentRun) {
+                return $scope.surveyData.runs[$scope.surveyData.currentRun];
+            }
+        };
+
+        $scope.startNewSurveyRun = function() {
+            $scope.surveyData.runs.push({
+                score: -1
+            });
+            $scope.surveyData.currentRun++;
+        };
+    });
