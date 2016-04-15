3e44f689e28639f06b72a53307b0bfcae80f3918
Ernst Verhoeven <E.J.Verhoeven@uva.nl>
2016-04-15 22:56:24 +0000

    OOPOCTVWO-12 somewhat reusable cardboard/fullscreen controls for Babylon help1
    git-svn-id: https://source.ic.uva.nl/repos/svn/odg-1/virtualmath/virtualmath/trunk@9176 89e93b6c-07a7-4196-abc3-db709d5d5625
    
    Former-commit-id: 08ffe197ff46ce91cd44d98a2349f835e70d145e

--- Babylon/js/gui-manager.js
+++ Babylon/js/gui-manager.js
@@ -1,8 +1,48 @@
-function goFullScreen() {
-    var element = document.documentElement;
-    element.requestFullscreen ? element.requestFullscreen() : element.mozRequestFullScreen ? element.mozRequestFullScreen() : element.webkitRequestFullscreen ? element.webkitRequestFullscreen() : element.msRequestFullscreen && element.msRequestFullscreen();
-}
+function GUIManager() {
+    var vm = this;
+    
+    function goFullScreen() {
+        var element = document.documentElement;
+        element.requestFullscreen ? element.requestFullscreen() : element.mozRequestFullScreen ? element.mozRequestFullScreen() : element.webkitRequestFullscreen ? element.webkitRequestFullscreen() : element.msRequestFullscreen && element.msRequestFullscreen();
+    }
 
-function goCardboard() {
-    document.location = "webvr-2.html";
+    function goWindowedScreen() {
+        document.exitFullscreen ? document.exitFullscreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitExitFullscreen && document.webkitExitFullscreen();
+    }
+
+    vm.isVRCameraSelected = false;
+
+    vm.selectVRCamera = function () {
+        vm.isVRCameraSelected = true;
+        cameraButton.onclick = vm.selectNormalCamera;
+        cameraButton.src = 'img/oculus.png';
+        window.redrawScene();
+    };
+    vm.selectNormalCamera = function () {
+        vm.isVRCameraSelected = false;
+        cameraButton.onclick = vm.selectVRCamera;
+        cameraButton.src = 'img/phone.png';
+        window.redrawScene();
+    };
+
+    vm.selectFullScreen = function () {
+        screenButton.onclick = vm.selectWindowedScreen;
+        screenButton.src = 'img/screen.png';
+        goFullScreen();
+    };
+
+    vm.selectWindowedScreen = function () {
+        screenButton.onclick = vm.selectFullScreen;
+        screenButton.src = 'img/fullscreen.png';
+        goWindowedScreen();
+    };
+
+    var cameraButton = document.getElementById('cameraButton');
+    cameraButton.src = 'img/oculus.png';
+    cameraButton.onclick = vm.selectVRCamera;
+
+    var screenButton = document.getElementById('screenButton');
+    screenButton.src = 'img/fullscreen.png';
+    screenButton.onclick = vm.selectFullScreen;
+
 }
