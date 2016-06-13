ba89527600e5fd513290579d516adbd071d92000
Tom Kuipers <T.F.Kuipers@uva.nl>
2016-06-13 08:35:57 +0000

    dependencies, dependencies. Buildserver has trouble finding suitable version of i18next. Works fine locally, retry.
    git-svn-id: https://source.ic.uva.nl/repos/svn/odg-1/virtualmath/virtualmath/trunk@9328 89e93b6c-07a7-4196-abc3-db709d5d5625
    
    Former-commit-id: c2dc86b5362ad2e39fe9abc65e7cdf1eccf97db1

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 angular.module('virtualMath', [
         'ngRoute',
         'ngTouch',
+        'ngSanitize',
         'virtualMath.start',
         'virtualMath.personalInfo',
         'virtualMath.intro',
